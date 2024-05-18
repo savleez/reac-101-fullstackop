6faccb001b0b4a446312f43de3962df074f50da9
sergio.vlezh <sergio.vlezh@gmail.com>
2024-05-18 19:46:30 +0000

    Apply format to App component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,10 +1,10 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
   const exercises1 = 10;
-  const part2 = "Using props to pass data";
+  const part2 = 'Using props to pass data';
   const exercises2 = 7;
-  const part3 = "State of a component";
+  const part3 = 'State of a component';
   const exercises3 = 14;
 
   return (
