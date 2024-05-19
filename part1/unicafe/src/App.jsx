98aeb604e1955cfc7aadd2069111172a5c2d32cf
sergio.vlezh <sergio.vlezh@gmail.com>
2024-05-19 18:21:23 +0000

    Add Button and Statistics components to handle the amount of good, bad and neutral votes.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,37 @@
 import { useState } from 'react';
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
-      <h1>Give Feedback</h1>
-      <button>Good</button>
-      <button>Neutral</button>
-      <button>Bad</button>
-      <br />
       <h1>Statistics</h1>
       <p>
         Good: {good} <br />
         Neutral: {neutral} <br />
         Bad: {bad}
       </p>
+    </>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onClickGood = () => setGood(good + 1);
+  const onClickNeutral = () => setNeutral(neutral + 1);
+  const onClickBad = () => setBad(bad + 1);
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <Button text='Good' onClick={onClickGood} />
+      <Button text='Neutral' onClick={onClickNeutral} />
+      <Button text='Bad' onClick={onClickBad} />
+      <br />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
