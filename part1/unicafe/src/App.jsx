3468ad1b28234cac9e61e1b44cf82dd846038629
sergio.vlezh <sergio.vlezh@gmail.com>
2024-05-19 17:53:18 +0000

    Add base that reflects the structure of the final results.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,25 @@
 import { useState } from 'react';
 
 const App = () => {
-  return <div>code here</div>;
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <br />
+      <h1>Statistics</h1>
+      <p>
+        Good: {good} <br />
+        Neutral: {neutral} <br />
+        Bad: {bad}
+      </p>
+    </>
+  );
 };
 
 export default App;
