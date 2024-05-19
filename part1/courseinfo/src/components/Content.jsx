f13f7a4406ceb420f68ab4fb254cbb414e05dc89
sergio.vlezh <sergio.vlezh@gmail.com>
2024-05-19 15:04:33 +0000

    Move Content component information to its own app Part

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,15 +1,11 @@
+import Part from './Part';
+
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.parts[0].name} {props.parts[0].exercises}
-      </p>
-      <p>
-        {props.parts[1].name} {props.parts[1].exercises}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exercises}
-      </p>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   );
 };
