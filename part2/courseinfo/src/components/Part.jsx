652a6d9bdcbe435b2b5ea19be2618ff4ca1bee3c
Sergio <svleez@gmail.com>
2024-05-26 13:35:23 -0500

    Update Part component to destructure the props

--- part2/courseinfo/src/components/Part.jsx
+++ part2/courseinfo/src/components/Part.jsx
@@ -1,8 +1,8 @@
-const Part = (props) => {
+const Part = ({ name, exercises }) => {
   return (
     <>
       <p>
-        {props.name} {props.exercises}
+        {name} {exercises}
       </p>
     </>
   );
